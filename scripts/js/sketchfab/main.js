86d59eec35bfb73b010db425a26a258be243707c
Leland Jobson <lelandjobson@gmail.com>
2019-02-21 16:27:11 -0500

    test homepage project cards idea

--- scripts/js/sketchfab/main.js
+++ scripts/js/sketchfab/main.js
@@ -3,6 +3,7 @@
     const sfUrl ='https://api.sketchfab.com/v3/models/'
 
     window.CreateProjectCard = function(projName, projDesc, projUrl, sfId){
+        // while(window.$ === null || window.$ === undefined){}
         let sfThumbUrl = sfUrl + sfId;
         fetch(sfThumbUrl)
         .then((response => response.json()))
@@ -11,12 +12,14 @@
             let thumb = data ? data.thumbnails.images[2].url : '';
             // Generate card
             let card = '';
-            card += '<div class="card">'
-            card += '<img class="card-img-top" src="'+ thumb + '">';
-            card += '<div class="card-body">'
-                card += '<h5 class="card-title">'+ projName +'</h5>';
+            card += '<div class="card card-home card-project">'
+                card += '<div class="card-header">'
+                card += '<a>' + projName + '</a>'
+                card += '</div>';
+                card += '<img class="card-img-top" src="'+ thumb + '">';
+                card += '<div class="card-body">'
                 card += '<p class="card-text">' + projDesc + '</p>';
-            card += '</div>';
+                card += '</div>';
             card += '</div>';
             $('#projCards').append(card);
         });
